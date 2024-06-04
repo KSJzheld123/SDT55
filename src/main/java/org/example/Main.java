397bc5953c69164865d685bd41a25fabc76a1ae4
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-06-04 16:43:02 +0900

    SDT55.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,25 @@ package org.example;
 class Main {
     public static void main(String[] args) {
 
+        boolean rs2 = 계산기.is_a_bigger_than_b(10, 20);
+        System.out.println(rs2);
+        rs2 = 계산기.is_a_bigger_than_b(240, 20);
+        System.out.println(rs2);
+        rs2 = 계산기.is_a_bigger_than_b(20, 20);
+        System.out.println(rs2);
 
     }
 }
 
 class 계산기 {
 
-    static int 합(int a, int b) {
+    static boolean is_a_bigger_than_b(int a, int b) {
+        boolean c = a > b;
+        return c;
     }
+
+
+
+
 
 }
